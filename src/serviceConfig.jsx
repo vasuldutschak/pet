459cedfe383b240160c783cfe8e9d7315772b8b6
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-09-30 15:01:29 +0200

    add config for service section

---
+++ src/serviceConfig.jsx
@@ -0,0 +1,11 @@
+import petGroomingImg from './assets/petGrooming.png'
+import petSittingImg from './assets/petSitting.png'
+import petAdoptionImg from './assets/petAdoption.png'
+import petServiceImg from './assets/petService.png'
+
+export default [
+    {image:petGroomingImg,title:"Pet Grooming",text:"There are many variations passages Lorem Ipsum available"},
+    {image:petSittingImg,title:"Pet Sitting",text:"There are many variations passages Lorem Ipsum available"},
+    {image:petAdoptionImg,title:"Pet Adoption",text:"There are many variations passages Lorem Ipsum available"},
+    {image:petServiceImg,title:"Veterinary Service",text:"There are many variations passages Lorem Ipsum available"},
+]
