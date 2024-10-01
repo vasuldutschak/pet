f664184284bc3be1416ca45bdd2da68897f0be4c
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-01 19:04:37 +0200

    add feedback section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import TakeCareSection from "../modules/take_care_section/TakeCareSection.jsx";
 import ServiceSection from "../modules/service_section/ServiceSection.jsx";
 import PetLoversSection from "../modules/pet_lovers_section/PetLoversSection.jsx";
 import PetsFoodSection from "../modules/accesories_section/PetsFoodSection.jsx";
+import FeedbackSection from "../modules/feedbackSection/FeedbackSection.jsx";
 
 function Home(props) {
     return (
@@ -10,6 +11,7 @@ function Home(props) {
             <ServiceSection/>
             <PetLoversSection/>
             <PetsFoodSection/>
+            <FeedbackSection/>
         </>
     );
 }
