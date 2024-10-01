32fbeb2bf6a8b3209bff1f5c42c0fbb52f739110
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-01 17:01:12 +0200

    add section to main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import TakeCareSection from "../modules/take_care_section/TakeCareSection.jsx";
 import ServiceSection from "../modules/service_section/ServiceSection.jsx";
 import PetLoversSection from "../modules/pet_lovers_section/PetLoversSection.jsx";
+import PetsFoodSection from "../modules/accesories_section/PetsFoodSection.jsx";
 
 function Home(props) {
     return (
@@ -8,6 +9,7 @@ function Home(props) {
             <TakeCareSection/>
             <ServiceSection/>
             <PetLoversSection/>
+            <PetsFoodSection/>
         </>
     );
 }
