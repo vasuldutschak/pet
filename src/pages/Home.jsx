f5fdbae2b4ce8bfeb4e794c4dfc31b47897f849d
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-01 15:32:27 +0200

    add section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import TakeCareSection from "../modules/take_care_section/TakeCareSection.jsx";
+import ServiceSection from "../modules/service_section/ServiceSection.jsx";
+import PetLoversSection from "../modules/pet_lovers_section/PetLoversSection.jsx";
 
 function Home(props) {
     return (
         <>
             <TakeCareSection/>
+            <ServiceSection/>
+            <PetLoversSection/>
         </>
     );
 }
