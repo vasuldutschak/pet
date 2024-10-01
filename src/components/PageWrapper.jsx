c6c6582cd587876b7471aa6388f5766ddd88c760
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-01 15:27:28 +0200

    change default with to 100%

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,12 +2,10 @@ import PropTypes from 'prop-types';
 import styled from "styled-components";
 
 const Container = styled.div`
-    max-width: 1400px;
     width: 100%;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
-    padding: 20px 10px;
     min-height: 100%;
 `
 
