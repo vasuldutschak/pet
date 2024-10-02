57efdb4aa1d99adbe2bdbf9d470a1edd13df1721
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-02 17:52:46 +0200

    add footer to layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 import {Suspense} from "react";
 import PageWrapper from "../components/PageWrapper.jsx";
 import Header from "../modules/header/Header.jsx";
+import Footer from "../modules/footer/Footer.jsx";
 
 function Layout(props) {
     return (
@@ -14,9 +15,7 @@ function Layout(props) {
                     </Suspense>
                 </div>
             </main>
-            <footer>
-                <h2>FOOOTERRR</h2>
-            </footer>
+            <Footer/>
         </PageWrapper>
     );
 }
