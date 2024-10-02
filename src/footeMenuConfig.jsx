deca774784be1f2bd9e31b3caefd115c15273e6a
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-02 17:51:04 +0200

    create footer menu config

---
+++ src/footeMenuConfig.jsx
@@ -0,0 +1,34 @@
+export default [
+    {
+        title: "Company", items: [
+            {link:"/our_story",text:"Our Story"},
+            {link:"/blog",text:"Blog"},
+            {link:"/career",text:"Career"},
+            {link:"/media",text:"Media"},
+        ]
+    },
+    {
+        title: "Quick Links", items: [
+            {link:"/products",text:"Products"},
+            {link:"/forms",text:"Forms"},
+            {link:"/help_desk",text:"Help Desk"},
+            {link:"/refund_policy",text:"Refund Policy"},
+        ]
+    },
+    {
+        title: "Difference", items: [
+            {link:"/our_promise",text:"Our Promise"},
+            {link:"/facilities",text:"Facilities"},
+            {link:"/meet_the_experts",text:"Meet the experts"},
+            {link:"/webcams",text:"Webcams"},
+            {link:"/commitment_to_safety",text:"Commitment to safety"},
+        ]
+    },
+    {
+        title: "Contact", items: [
+            {link:"/terms_&_Conditions",text:"Terms & Conditions"},
+            {link:"/privacy_policy",text:"Privacy Policy"},
+        ]
+    },
+
+]
