9f6b58494a6701b7500aee2b2d3fd3777f111b39
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-10-02 17:50:45 +0200

    create link config

---
+++ src/linksConfig.jsx
@@ -0,0 +1,9 @@
+import {RiInstagramFill} from "react-icons/ri";
+import {FaFacebookF, FaGooglePlusG, FaTwitter} from "react-icons/fa6";
+
+export default [
+    {icon:RiInstagramFill,link:"http://localhost:5173/instagram"},
+    {icon:FaFacebookF,link:"http://localhost:5173/facebook"},
+    {icon:FaTwitter,link:"http://localhost:5173/twitter"},
+    {icon:FaGooglePlusG,link:"http://localhost:5173/googleplus"},
+]
